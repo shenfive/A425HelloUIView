fb8d532ab6383c16e9624f1cdf9b83d41c363a36
Shen Jun Wu <shenfive@gmail.com>
2021-04-25 11:24:00 +0800

    init

--- A425HelloUIView/ViewController.swift
+++ A425HelloUIView/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0.0
+    var timer:Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let newView = UIView.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        
+        newView.backgroundColor = UIColor.red
+        
+        self.view.addSubview(newView)
+        
+        
+        
+        
     }
 
 
